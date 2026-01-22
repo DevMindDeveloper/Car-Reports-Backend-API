df4a29f7987e6c783538750a0169a4e53666f8ec
DevMindDeveloper <143184508+DevMindDeveloper@users.noreply.github.com>
2026-01-22 15:47:44 +0500

    Delete extra directory

--- extra/old_versions/utils.py
+++
@@ -1,9 +0,0 @@
-from dateutil import parser
-
-## date formating
-def convert_date_to_desired_format(date):
-    try:
-        dt = parser.parse(date)
-        return dt.strftime("%Y-%m-%d")
-    except Exception as e:
-        return f"Invlaid date : {e}"
